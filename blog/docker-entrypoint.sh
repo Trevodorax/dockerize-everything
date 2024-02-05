2d67beba61d8e226da6e4d92cc4725a89a83e661
Paul Gaudeaux <phvcgdx@gmail.com>
2024-02-05 14:36:12 +0100

    dockerize-rust-blog: wait for db before doing migrations

--- blog/docker-entrypoint.sh
+++ blog/docker-entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+# wait for the database to be ready
+until PGPASSWORD=meau-de-pace psql -h "db" -U "paul" -d "blog" -c '\q' 2>/dev/null; do
+  >&2 echo "Database is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Database is up - executing command"
 # run diesel migrations
-# TODO: wait for database to be ready before applying migrations
 diesel migration run
+
 # start the application
 exec "$@"
