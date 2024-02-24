85f761ab5244ba17eb995eb444bd5156284c4dfb
Paul Gaudeaux <phvcgdx@gmail.com>
2024-02-24 11:16:11 +0100

    improve-blog-performance-and-weight: use healthcheck for database dependency

--- blog/docker-entrypoint.sh
+++ blog/docker-entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Wait for the database to be ready
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "db" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q' 2>/dev/null; do
-  >&2 echo "Waiting for db..."
-  sleep 1
-done
 
 # Run migrations
 >&2 echo "Running migrations"
